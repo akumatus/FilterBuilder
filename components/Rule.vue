50eaad0c5a43a3f0845ad622c762383dc676764d
wuyue <wuyue07@baidu.com>
2016-12-16 00:38:16 +0800

    add fill form status logic

--- components/Rule.vue
+++ components/Rule.vue
@@ -1,24 +1,24 @@
 <template>
-  <div class="form-group and-or-rule">
-    <div class=" col-sm-3">
+  <div class="form-group and-or-rule col-xs-12">
+    <div class="col-xs-3">
       <select class="form-control input-sm" v-model="key">
-        <option v-for="option in options.keys.options" :value="option.key">
+        <option v-for="option in options.keys" :value="option.id">
           {{option.name}}
         </option>
       </select>
     </div>
 
-    <div class="col-sm-3">
-      <select class="form-control input-sm" v-model="condition">
-        <option v-for="option in options.conditions.options" :value="option.value">
+    <div class="col-xs-3">
+      <select class="form-control input-sm" v-model="operator">
+        <option v-for="option in options.operators" :value="option.id">
           {{option.name}}
         </option>
       </select>
     </div>
 
-    <div class="col-sm-3">
-      <label class="sr-only">Value</label>
-      <input type="text" class="form-control input-sm" v-model="value" placeholder="Value">
+    <div class="col-xs-3">
+      <label class="sr-only">值</label>
+      <input type="text" class="form-control input-sm" v-model="value" placeholder="值">
     </div>
 
     <button class="btn btn-xs btn-purple-outline btn-radius btn-purple-round" @click.prevent="deleteSelf()">
@@ -42,13 +42,27 @@
     data () {
       return {
         key: -99,
-        condition: -99,
+        operator: -99,
         value: ''
       }
     },
     methods: {
       deleteSelf () {
         this.$emit('delete-rule');
+      },
+
+      queryFormStatus () {
+        return {
+          'key': this.key,
+          'operator': this.operator,
+          'value': this.value
+        }
+      },
+
+      fillRuleStatus (data) {
+        this.key = data.key;
+        this.operator = data.operator;
+        this.value = data.value;
       }
     }
   }
@@ -59,6 +73,7 @@
     position: relative;
     height: 30px;
     margin-left: 15px !important;
+    padding-left: 0;
   }
 
   .and-or-rule:before,
@@ -70,6 +85,7 @@
     height: calc(50% + 15px);
     border-color: #c0c5e2;
     border-style: solid;
+    z-index: -1;
   }
 
   .and-or-rule:before {
